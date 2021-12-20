24c82ac72ac98e73e7269f170a933deea9c23681
Cartago <cartago.relaxingcup@gmail.com>
2021-12-20 07:44:17 +0100

    Prueba con otra dependencia para el problema de las rutas de react

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-const path = process.cwd();
+const path = require("path");
 const { dbConnection } = require("./database/config");
 
 // console.log(process.env);
